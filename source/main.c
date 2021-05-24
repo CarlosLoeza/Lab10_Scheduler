0ecc5b99c381a5d77e18c581705fbcfa22a5d45c
CarlosLoeza <loeza.carlos@yahoo.com>
2021-05-23 20:17:26 -0700

    Pre-Lab fixed

--- source/main.c
+++ source/main.c
@@ -7,170 +7,58 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clea$
 
-// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
-unsigned long _avr_timer_M = 1; // Start counter from here to 0. Default 1ms
-unsigned long _avr_timer_cntcurr = 0; // Current internal clock of 1ms ticks
+/*
+unsigned char x;
+unsigned char light;
 
-void TimerOn(){
-    // AVR timer/counter controller register TCCR1
-    TCCR1B = 0x0B;      // bit3 = 0: CTC mode(clear timer on compare)
-                        // bit2bit1bit0 = 011;
-                        // 0000 1011 : 0x0B
-                        // So 8 MHz clock or 8,000,000 / 64  = 125,000 ticks
-                        // Thus TCNT1 register will count 125,000 ticks
+enum Button_States {Button_Start, Button_Wait, Button_Pressed, Button_Held} Button_State;
 
-    // AVR out compare register OCR1A
-    OCR1A = 125;        // Timer interrupt will be generated when TCNT1==OCR1A
-                        // We want a 1ms tick. .001s * 125,000 ticks = 125
-                        // So when TCNT1 == 125 then that means 1ms has passed
-
-    // AVR timer register interrupt mask register
-    TIMSK1 = 0x02;      // bit1: OCIE1A -- enables compare match register
-
-    // Initiliaze AVR counter
-    TCNT1 = 0;
-
-    _avr_timer_cntcurr = _avr_timer_M;
-
-    // enable global interrupts
-    SREG |= 0x80;       // 1000 0000 
-}
-
-void TimerOff(){
-    TCCR1B = 0x00;
-}
-
-void TimerISR(){
-    TimerFlag = 1;
-}
-
-// In our approach C program does not touch this ISR, only TimerISR()
-ISR(TIMER1_COMPA_vect){
-    // CPU automatically calls when TCNT1 == OCR1 (Every 1ms per TimerOn settings)
-    _avr_timer_cntcurr--;       // count down to 0
-    if (_avr_timer_cntcurr == 0){
-        TimerISR();     // call the ISR that the user uses
-        _avr_timer_cntcurr = _avr_timer_M;
-    }
-}
-
-// Set timer to tick every M ms.
-void TimerSet(unsigned long M){
-    _avr_timer_M = M;
-    _avr_timer_cntcurr =  _avr_timer_M;
-}
-
-
-unsigned char led0_output = 0x00;
-unsigned char led1_output= 0x00;
-unsigned char pause = 0;
-
-
-enum toggleLED0_States {toggleLED0_wait, toggleLED0_blink};
-
-int toggleLED0SMTick(int state){
-    switch(state){
-	case toggleLED0_wait: state = !pause? toggleLED0_blink: toggleLED0_wait; break;
-	case toggleLED0_blink: state = pause? toggleLED0_wait: toggleLED0_wait; break;
-	default: state = toggleLED0_wait; break;
-    }
-
-    switch(state){
-	case toggleLED0_wait: break;
-	case toggleLED0_blink:
-	    led0_output = (led0_output == 0x00) ? 0x01 : 0x00;
-	    break;
-    }
-    return state;
-}
-
-enum toggleLED1_States {toggleLED1_wait, toggleLED1_blink};
-
-int toggleLED1SMTick(int state){
-    switch(state){
-        case toggleLED1_wait: state = !pause? toggleLED1_blink: toggleLED1_wait; break;
-        case toggleLED1_blink: state = pause? toggleLED1_wait: toggleLED1_wait; break;
-        default: state = toggleLED1_wait; break;
-    }
-
-    switch(state){
-        case toggleLED1_wait: break;
-        case toggleLED1_blink:
-            led0_output = (led1_output == 0x00) ? 0x01 : 0x00;
+void Button(){
+    unsigned char button = x;
+    
+    switch(Button_State){
+        case Button_Start:
+            Button_State = Button_Wait;
             break;
-
+        case Button_Wait:
+            if(button)
+                Button_State = Button_Pressed;
+            else
+                Button_State = Button_Wait;
+            break;
+        case Button_Pressed:
+            Button_State = Button_Pressed;
+            break;
+        case Button_Held:
+            if(button)
+                Button_State = Button_Held;
+            else
+                Button_State = Button_Wait;
+            break;
+        default:
+            Button_State = Button_Wait;
+            
     }
-    return state;
+    
+    switch (Button_State) {
+        case Button_Pressed:
+            light = 0x01;
+            break;
+        case Button_Held:
+            light = 0x01;
+            break;
+    }
+    
 }
-
-
-enum display_States{ display_display};
-
-int displaySMTick(int state){
-    unsigned char output;
-
-    switch(state){
-	case display_display: state = display_display; break;
-   	default: state = display_display; break;
-    }
-
-    switch(state){
-	case display_display:
-	    output = led0_output | led1_output << 1;
-	    break;
-    }
-    PORTB = output;
-    return state;
-}
-
-
-enum pauseButtoSM_State {pauseButton_wait, pauseButton_press, pauseButton_release};
-
-// Monitors buttons connected to PA0
-// When button is pressed, shared variable "pause" is toggled
-int pauseButtonSMTick(int state){
-    // Local Variables
-    unsigned char press = ~PINA & 0x01;
-   
-    switch(state){
-	case pauseButton_wait:
-	    state = press == 0x01? pauseButton_press: pauseButton_wait; break;
-	case pauseButton_press:
-	    state = pauseButton_release; break;
-	case pauseButton_release:
-	    state = press == 0x00? pauseButton_wait: pauseButton_press; break;
-	default:
-	    state = pauseButton_wait; break;
-    }
-
-    switch(state){
-	case pauseButton_wait: break;
-	case pauseButton_press:
-	    pause = (pause == 0) ? 1:0;
-	    break;
-	case pauseButton_release: break;
-    }
-    return state;
-
-}
-
-// -- Task Scheduler --
-// Represents runnings process in our simple real time operating system
-typedef struct task{
-    signed char state;
-    unsigned long int period;
-    unsigned long int elapsedTime;
-    int (*TickFct)(int);
-
-} task;
+*/
 
 unsigned char GetBit(unsigned char x, unsigned char k) {
    return ((x & (0x01 << k)) != 0);
@@ -180,105 +68,75 @@ unsigned char GetKeypadKey(){
     PORTC = 0xEF;
     asm("nop");
     if(GetBit(PINC,0)==0) {return('1');}
-    if(GetBit(PINC,0)==0) {return('4');} 
-    if(GetBit(PINC,0)==0) {return('7');} 
-    if(GetBit(PINC,0)==0) {return('*');} 
+    if(GetBit(PINC,1)==0) {return('4');}
+    if(GetBit(PINC,2)==0) {return('7');}
+    if(GetBit(PINC,3)==0) {return('*');}
  
     // check column 2 numbers
     PORTC = 0xDF;
-    asm("nop"); 
+    asm("nop");
     if(GetBit(PINC,0)==0) {return('2');}
-    if(GetBit(PINC,0)==0) {return('5');} 
-    if(GetBit(PINC,0)==0) {return('8');} 
-    if(GetBit(PINC,0)==0) {return('0');}
-
+    if(GetBit(PINC,1)==0) {return('5');}
+    if(GetBit(PINC,2)==0) {return('8');}
+    if(GetBit(PINC,3)==0) {return('0');}
     PORTC = 0xBF;
-    asm("nop"); 
+    asm("nop");
     if(GetBit(PINC,0)==0) {return('3');}
-    if(GetBit(PINC,0)==0) {return('6');} 
-    if(GetBit(PINC,0)==0) {return('9');} 
-    if(GetBit(PINC,0)==0) {return('#');} 
+    if(GetBit(PINC,1)==0) {return('6');}
+    if(GetBit(PINC,2)==0) {return('9');}
+    if(GetBit(PINC,3)==0) {return('#');}
  
     PORTC = 0x7F;
-    asm("nop"); 
+    asm("nop");
     if(GetBit(PINC,0)==0) {return('A');}
-    if(GetBit(PINC,0)==0) {return('B');} 
-    if(GetBit(PINC,0)==0) {return('C');} 
-    if(GetBit(PINC,0)==0) {return('D');}
-
-    return ('\0'); 
-
-
-}
+    if(GetBit(PINC,1)==0) {return('B');}
+    if(GetBit(PINC,2)==0) {return('C');}
+    if(GetBit(PINC,3)==0) {return('D');}
+    return ('\0');
+ }
 
 
-unsigned long int findGCD(unsigned long int a, unsigned long int b){
-    unsigned long int c;
-    while(1){
-	c = a%b;
-	if(c==0) {return b;}
-    	a=b;
-	b=c;
-    }
-    return 0;
-}
+ int main(void) {
+     /* Insert DDR and PORT initializations */
+          unsigned char x;
+          DDRB = 0xFF; PORTB = 0x00;
+	  DDRC = 0xF0; PORTC = 0x0F;
+          /* Insert your solution below */
+          while (1) {
+              x = GetKeypadKey();
+              switch(x){
+                  case '\0': PORTB = 0x1F; break;
+                  case '1': PORTB = 0x01; break;
+                  case '2': PORTB = 0x02; break;
+                  case '3': PORTB = 0x03; break;
+                  case '4': PORTB = 0x04; break;
+                  case '5': PORTB = 0x05; break;
+                  case '6': PORTB = 0x06; break;
+                  case '7': PORTB = 0x07; break;
+                  case '8': PORTB = 0x08; break;
+                  case '9': PORTB = 0x09; break;
+                  case 'A': PORTB = 0x0A; break;
+                  case 'B': PORTB = 0x0B; break;
+                  case 'C': PORTB = 0x0C; break;
+                  case 'D': PORTB = 0x0D; break;
+                  case '*': PORTB = 0x0E; break;
+                  case '0': PORTB = 0x00; break;
+                  case '#': PORTB = 0x0F; break;
+                  default: PORTB = 0x1B; break;
+              }
+          }
+     
+     
+//     /* Insert DDR and PORT initializations */
+//     DDRB = 0xFF; PORTB = 0x00;
+//     DDRC = 0x00; PORTC = 0xFF;
+//
+//
+//     /* Insert your solution below */
+//     while (1) {
+//         x = GetKeypadKey();
+//         Button();
+//         PORTA = light;
+//     }
+ }
 
-//unsigned long GCD = tasks[0]->period;
-//for(int i = 1; i<numTasks; i++){ GCD = findGCD(GCD, tasks[i]->period);}
-
-
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
-
-    static task task1, task2, task3, task4;
-    task *tasks[] = {&task1, &task2, &task3, &task4};
-    const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
-
-    const char start = -1;
-    // Task1 { pauseButtonToggleSM}
-    task1.state = start; // init state
-    task1.period = 50;
-    task1.elapsedTime = task1.period;
-    task1.TickFct = &pauseButtonSMTick;
-    // Task2 { toggleLED0SM}
-    task2.state = start; // init state
-    task2.period = 500;
-    task2.elapsedTime = task2.period;
-    task2.TickFct = &toggleLED0SMTick;
-    // Task3 { toggleLED1SM}
-    task3.state = start; // init state
-    task3.period = 1000;
-    task3.elapsedTime = task3.period;
-    task3.TickFct = &toggleLED1SMTick;
-    // Task4 { displaySM }
-    task4.state = start; // init state
-    task4.period = 10;
-    task4.elapsedTime = task4.period;
-    task2.TickFct = &displaySMTick;
-
-    unsigned long GCD = tasks[0]->period;
-    for(int i = 1; i<numTasks; i++){ GCD = findGCD(GCD, tasks[i]->period);}
-
-    TimerSet(GCD);
-    TimerOn();
-
-
-    unsigned short i;
-    while(1){
-//	unsigned long GCD = tasks[0]->period;
-//	for(int i = 1; i<numTasks; i++){ GCD = findGCD(GCD, tasks[i]->period);}
-	for(i =0; i<numTasks; i++){
-	    if(tasks[i]->elapsedTime == tasks[i]->period){
-		tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
-		tasks[i]->elapsedTime = 0;
-	    }
-	    tasks[i]->elapsedTime += GCD;
-	}
-	while(!TimerFlag);
-	TimerFlag = 0;
-    }
-    return 0;
-}
-  
